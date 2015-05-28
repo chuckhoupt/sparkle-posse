9d573662d5769026921df13ed585ba9ba0995122
Chuck Houpt <chuck@habilis.net>
2015-05-28 11:57:06 -0400

    Cleaned-up/expanded config-template doc.

--- config-template.php
+++ config-template.php
@@ -1,25 +1,40 @@
 <?php
-// Configuration
+/*
 
-	/* Required Log glob pattern:
-	     log_glob_pattern - a glob pattern to feed to zgrep to extract 1-4 weeks of logs.
-	   
-	   Patterns for common WebHosts:
-	   
-	     DreamHost: 
-$root_path = explode('/', $_SERVER['DOCUMENT_ROOT']);
-$user = $root_path[2]; $host = $root_path[3];
+Sparkle Posse Configuration
+
+The only required configuration parameter is log_glob_pattern. The default below is
+for a typical plain vanila Apache webhost setup (such as OS X Server). The exact glob
+pattern will depend on your server/webhost and log-rotation setup.
+
+Sparkle Posse uses the glob pattern to z-greps the logs for Sparkle profile checks.
+In a shell, you can test your glob pattern the same way in order to check that the
+glob lists at least 7 days:
+
+% zgrep Sparkle /my-log-path/access.*
+
+Log glob patterns for common WebHosts:
+
+DreamHost:
+
+$user = 'me';
+$host = 'example.com';
 $log_path = "/home/$user/logs/$host/http";
 $config['log_glob_pattern'] = "$log_path/access.log.2* $log_path/access.log";
 
-	*/
+*/
+
+/*
+ * Required Log glob pattern:
+ *	     log_glob_pattern - a glob pattern to feed to zgrep to extract 1-4 weeks of logs.
+*/
 $config['log_glob_pattern'] = "/var/log/apache2/access_log*";
 
 /*
- * Optional "André the Giant has a posse" Parody Sticker:
- *   appIcon - Large (256px+) app icon to be rendered xerox-style in the sticker.
+ * Optional Shepard Fairey style "André the Giant Has a Posse" parody sticker:
+ *   appIcon - URL of large (256px+) app icon to be rendered xerox-style in the sticker.
  *   ringName - Optional 3-line ring-name (wrestling alias) for app.
- *              (Use non-break spaces for indenting) 
+ *              (Use non-break spaces for indenting)
 */
 //$config['appIcon'] = "Sparkle.png";
 //$config['ringName'] = ["Sparke", "   the", "Updater"];
@@ -27,5 +42,7 @@ $config['log_glob_pattern'] = "/var/log/apache2/access_log*";
 /*
  * Optional Server-side GeoIP support:
  *   geoip_city_db - path to MaxMind's GeoIP2 database
+ *
+ * Note: requires GeoIP2 package and DB. See https://github.com/maxmind/GeoIP2-php
 */
 //$config['geoip_city_db'] = "/usr/local/var/GeoIP/GeoLite2-City.mmdb";
