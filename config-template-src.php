03c53ea4b01877726fb49141485e99febe0cd3c8
Chuck Houpt <chuck@habilis.net>
2015-05-28 11:16:34 -0400

    Added demo link to config template source listing.

---
+++ config-template-src.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+	<META CHARSET="utf-8">
+	<META NAME="viewport" CONTENT="width=device-width, initial-scale=1">
+	<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+	<TITLE>Config Template</TITLE>
+</HEAD>
+<BODY>
+<?php highlight_file( 'config-template.php' ); ?>
+</BODY>
+</HTML>
